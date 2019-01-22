27720cc669eaafd57eb590fbe98e0486a07f7dcb
jyce3d <42608645+jyce3d@users.noreply.github.com>
2019-01-22 17:34:21 +0100

    Update usage.js

--- usage.js
+++ usage.js
@@ -1,2 +1,14 @@
 var singleton = require ("./DBConSingleton.js");
+var build = require("./StudyBuilder");
 var dbConn = singleton.getInstance();
+var studyBuilder = StudyBuilder(dbConn, {
+    studyId : "14"
+});
+var study = StudyBuilder.getStudy( {
+    includeCohorts:true,
+    includePatients:false
+});
+
+var cohorts = StudyBuilder.getCohorts({
+    includePatients:true
+});
